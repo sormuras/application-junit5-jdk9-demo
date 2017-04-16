3c37ff1e5f782bb93366202a58a4678dbb38a69c
Christian Stein <sormuras@gmail.com>
2017-04-16 18:36:11 +0200

    Pass hard-coded command line options to JUnit ConsoleLauncher

--- src/main/java/integration/integration/ConsoleLauncherIntegration.java
+++ src/main/java/integration/integration/ConsoleLauncherIntegration.java
@@ -3,7 +3,11 @@ package integration;
 import org.junit.platform.console.ConsoleLauncher;
 
 public class ConsoleLauncherIntegration {
-  public static void main(String[] args) {
-    ConsoleLauncher.main(args);
-  }
+    public static void main(String[] args) {
+        ConsoleLauncher.main(
+                "--scan-classpath",
+                "--classpath", "target/idea/production/application.api",
+                "--classpath", "target/idea/production/integration"
+        );
+    }
 }
